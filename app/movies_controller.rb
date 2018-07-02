1aadbfff6a10043866fbc784f3fa064605e3cbf7
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-02 17:04:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,8 +74,9 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie.update_all({:title => 'Wat, huh?'}
+  Movie.update_all({:title => 'Wat, huh?'})
 end
+
 
 def can_update_multiple_items_at_once
   # Change title of all movies to "A Movie"
